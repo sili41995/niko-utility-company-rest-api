0aa84cd1cb64e8641ed2a42ab4d2085a7d801b75
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-08 21:51:53 +0300

    Update auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -37,39 +37,6 @@ class AuthService {
 
     return result;
   }
-
-  async addUser(data: NewUser): Promise<UserData> {
-    const { login, password } = data;
-    const user = await prisma.user.findUnique({ where: { login } });
-
-    if (user) {
-      throw httpError({
-        status: 409,
-        message: ErrorMessages.duplicateLoginErr,
-      });
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-    const result = await prisma.user.create({
-      data: { ...data, password: hashPassword },
-      select: {
-        id: true,
-        name: true,
-        login: true,
-        email: true,
-        fullAccess: true,
-        houses: true,
-        subscribers: true,
-        accounting: true,
-        documents: true,
-        counters: true,
-        oneOffJobs: true,
-        settings: true,
-      },
-    });
-
-    return result;
-  }
 }
 
 export default AuthService;
