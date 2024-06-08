fe9424ffb9ae35b2c3cb56268ad3549824d63d51
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-08 11:30:04 +0300

    create project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
+import { IHttpError } from "./types/types";
+import { authRouter, contactsRouter } from "./routes/api";
+import { PrismaClient } from "@prisma/client";
+import AppRouter from "@/routes";
+
+const app: Express = express();
+const router = new AppRouter(app);
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+export const prisma = new PrismaClient();
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+router.init();
+
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+app.use((err: IHttpError, req: Request, res: Response, next: NextFunction): void => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app, { prisma } from "@/app";
+
+const port = process.env.PORT || 3000;
+
+prisma
+  .$connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Now listening on port ${port}`);
+    });
+  })
+  .catch(() => {
+    prisma.$disconnect();
+  });
