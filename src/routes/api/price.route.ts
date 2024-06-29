cd602f7241b5f2ac74012bbcc4e822458696133c
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-29 20:32:56 +0300

    Update price.route.ts

--- src/routes/api/price.route.ts
+++ src/routes/api/price.route.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { Endpoints } from '../../constants';
 import { auth, ctrlWrapper } from '../../middlewares';
-import priceController from '../../controllers/price.controller';
+// import priceController from '../../controllers/price.controller';
 
 const router: Router = Router();
 
 router.use(auth);
 
-const calculatePricesCtrl = ctrlWrapper(priceController.getAll.bind(priceController));
+// const calculatePricesCtrl = ctrlWrapper(priceController.getAll.bind(priceController));
 
-router.get(Endpoints.root, calculatePricesCtrl);
+// router.get(Endpoints.root, calculatePricesCtrl);
 
 export default router;
